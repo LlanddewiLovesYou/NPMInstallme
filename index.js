b901cc47d0234826a736cf939195427bc9f478d0
LlanddewiLovesYou <idelduca@stridenyc.com>
2019-01-02 19:39:57 -0500

    Revamp of info, update of printout, add colors

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ fs.readFile("./info.json", "utf8", function(err, data) {
   if (err) {
     return console.log(err);
   }
-  console.log(data);
+  const object = JSON.parse(data);
+  console.log("\n");
+  Object.keys(object).forEach(key => {
+    key === "name" || key === "occupation" || key === "message"
+      ? console.log("\x1b[34m\x1b[1m" + `${object[key]}` + "\x1b[0m")
+      : console.log("\x1b[31m\x1b[1m" + `${key}: ${object[key]}` + "\x1b[0m");
+  });
+  console.log("\n");
 });
